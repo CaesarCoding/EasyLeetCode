b72ad62361418663e189f8c5340efe13d8ec6074
Caesar <pkorochi@qq.com>
2022-03-06 23:01:31 +0800

    containsDuplicate

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,23 @@
+import solution.ContainsDuplicate
 import solution.MaxProfit
 import solution.RemoveDuplicates
 import solution.Rotate
 
 fun main(args: Array<String>) {
-    executeSolution(ROTATE)
+    executeSolution(CONTAINS_DUPLICATE)
 }
 
 const val REMOVE_DUPLICATES = "RemoveDuplicates"
 const val MAX_PROFIT = "MaxProfit"
 const val ROTATE = "Rotate"
+const val CONTAINS_DUPLICATE = "ContainsDuplicate"
 
 fun executeSolution(name: String) {
     val solution = when (name) {
         REMOVE_DUPLICATES -> RemoveDuplicates()
         MAX_PROFIT -> MaxProfit()
         ROTATE -> Rotate()
+        CONTAINS_DUPLICATE -> ContainsDuplicate()
         else -> null
     }
 
